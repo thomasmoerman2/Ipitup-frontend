dd8186e9ffb5783a87f446a20a29e68bde75b2ef
mats-verbeke <mats.verbeke@student.howest.be>
2025-01-15 09:20:06 +0100

    Search profile componnent

---
+++ src/components/Search/Profile.vue
@@ -0,0 +1,29 @@
+<template>
+  <button class="flex items-center justify-between h-max">
+    <div class="flex items-center justify-between h-max">
+      <div class="flex items-center">
+        <img :src="img" alt="image" class="w-[57px] h-[57px] rounded aspect-circle"/>
+        <p class="font-bold">{{ name }}</p>
+      </div>
+      <p class="text-xs text-black-40">{{ username }}</p>
+    </div>
+  </button>
+</template>
+
+<script setup>
+
+const props = defineProps({
+  img: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+});
+</script>
