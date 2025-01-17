e7817b846eac9c491b623eb0e5f2a2eccefe477a
mats-verbeke <mats.verbeke@student.howest.be>
2025-01-17 09:14:20 +0100

    profile badges added

--- src/views/profile.vue
+++ src/views/profile.vue
@@ -20,16 +20,23 @@
 
     <div>
       <p class="font-bold">Moving time</p>
-      <div class="w-fit whitespace-nowrap">
+      <div class="w-fit whitespace-nowrap py-3">
         <AppOptions :options="[
           { text: 'Deze week', value: 'Deze week' },
           { text: 'Deze maand', value: 'Deze maand' }
         ]" v-model="selectedOption" @change="handleOptionChange" />
       </div>
+      <div class="flex w-full bg-blue-6 h-[10rem] justify-center items-center text-center">[Grafiek]</div>
     </div>
 
     <div>
-      <p class="font-bold">Badges</p>
+      <p class="font-bold pb-3">Badges</p>
+      <div class="flex flex-wrap justify-center gap-2">
+          <AppBadge exercise="Pushup" amount="100" />
+          <AppBadge exercise="Squat" amount="500" />
+          <AppBadge exercise="Pullup" amount="200" />
+          <AppBadge exercise="Situp" amount="50" />
+        </div>
     </div>
   </div>
 
@@ -40,6 +47,7 @@ import SettingsAvatar from "@/components/Settings/Avatar.vue";
 import AppSmallButton from "@/components/App/SmallButton.vue";
 import ProfileInfo from "@/components/Profile/Info.vue";
 import AppOptions from "@/components/App/Options.vue";
+import AppBadge from "@/components/App/Badge.vue";
 import { ref } from 'vue';
 
 const selectedOption = ref('1');
