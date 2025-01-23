aec3095e781982392971824976f611b4d4370abb
mats-verbeke <mats.verbeke@student.howest.be>
2025-01-23 23:19:29 +0100

    nonstopbadge

--- src/components/App/Icon.vue
+++ src/components/App/Icon.vue
@@ -1,30 +1,30 @@
 <template>
-    <Eye v-if="props.name === 'eye'" :class="color" :size="size" />
-    <EyeOff v-if="props.name === 'eyeOff'" :class="color" :size="size" />
-    <Search v-if="props.name === 'search'" :class="color" :size="size" />
-    <Camera v-if="props.name === 'camera'" :class="color" :size="size" />
-    <Pen v-if="props.name === 'pen'" :class="color" :size="size" />
+    <Eye v-if="props.name === 'eye'" :class="color" :size="size" :fill="fill"/>
+    <EyeOff v-if="props.name === 'eyeOff'" :class="color" :size="size" :fill="fill"/>
+    <Search v-if="props.name === 'search'" :class="color" :size="size" :fill="fill"/>
+    <Camera v-if="props.name === 'camera'" :class="color" :size="size" :fill="fill"/>
+    <Pen v-if="props.name === 'pen'" :class="color" :size="size" :fill="fill"/>
     <ArrowUpRight v-if="props.name === 'arrowUpRight'" :class="color" :size="size" />
     <User v-if="props.name === 'User'" :class="color" :size="size" />
     <UserRoundMinus v-if="props.name === 'UserRoundMinus'" :class="color" :size="size" />
-    <Award v-if="props.name === 'Award'" :class="color" :size="size" />
+    <Award v-if="props.name === 'Award'" :class="color" :size="size" :fill="fill"/>
     <RefreshCw v-if="props.name === 'RefreshCw'" :class="color" :size="size" />
-    <Gem v-if="props.name === 'Gem'" :class="color" :size="size" />
-    <Globe v-if="props.name === 'Globe'" :class="color" :size="size" />
-    <Star v-if="props.name === 'Star'" :class="color" :size="size" />
+    <Gem v-if="props.name === 'Gem'" :class="color" :size="size" :fill="fill"/>
+    <Globe v-if="props.name === 'Globe'" :class="color" :size="size" :fill="fill"/>
+    <Star v-if="props.name === 'Star'" :class="color" :size="size" :fill="fill"/>
     <ChevronRight v-if="props.name === 'ChevronRight'" :class="color" :size="size" />
     <ChevronsUpDown v-if="props.name === 'ChevronsUpDown'" :class="color" :size="size" />
     <Timer v-if="props.name === 'Timer'" :class="color" :size="size" />
     <X v-if="props.name === 'X'" :class="color" :size="size" />
     <Filter v-if="props.name === 'Filter'" :class="color" :size="size" />
     <ArrowLeft v-if="props.name === 'ArrowLeft'" :class="color" :size="size" />
-    <Flame v-if="props.name === 'Flame'" :class="color" :size="size" />
-    <Dumbbell v-if="props.name === 'Dumbbell'" :class="color" :size="size" />
-    <KeyRound v-if="props.name === 'KeyRound'" :class="color" :size="size" />
-    <UserCircle2 v-if="props.name === 'UserCircle2'" :class="color" :size="size" />
+    <Flame v-if="props.name === 'Flame'" :class="color" :size="size" :fill="fill"/>
+    <Dumbbell v-if="props.name === 'Dumbbell'" :class="color" :size="size" :fill="fill"/>
+    <KeyRound v-if="props.name === 'KeyRound'" :class="color" :size="size" :fill="fill"/>
+    <UserCircle2 v-if="props.name === 'UserCircle2'" :class="color" :size="size" :fill="fill"/>
     <Check v-if="props.name === 'Check'" :class="color" :size="size" />
-    <Lock v-if="props.name === 'Lock'" :class="color" :size="size" />
-    <Atom v-if="props.name === 'Atom'" :class="color" :size="size" />
+    <Lock v-if="props.name === 'Lock'" :class="color" :size="size" :fill="fill"/>
+    <Atom v-if="props.name === 'Atom'" :class="color" :size="size" :fill="fill"/>
 </template>
 
 <script setup>
@@ -37,6 +37,10 @@ const props = defineProps({
     },
     color: {
         type: String,
+    },
+    fill: {
+        type: String,
+        default: 'none'
     },
     size: {
         type: Number,
